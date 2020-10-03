8a6bde208808968ff69f79e306e83f43bf28962a
YERIM <dnaer456@gmail.com>
2020-10-03 19:05:42 +0900

    screen

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import { Route } from "react-router-dom";
 import Navigation from "./Layouts/Navigation";
 import Want from "./Screens/Want";
 import Sleep from "./Screens/Sleep";
+import Button from "./Button";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Route path="/" component={Navigation}></Route>
+
+        <Route path="/want" component={Want}></Route>
+
+        <Route path="/sleep" component={Sleep}></Route>
       </div>
     );
   }
