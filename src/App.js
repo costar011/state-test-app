17b2cd4be3c2c1e391456ab626dba45a2a6a5267
YERIM <dnaer456@gmail.com>
2020-10-18 22:39:18 +0900

    btn

--- src/App.js
+++ src/App.js
@@ -8,9 +8,21 @@ import Button from "./Button";
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      changeValue: 0,
+    };
+  }
   render() {
     return (
       <div>
+
+        <h1>{this.state.changeValue}</h1>
+        <Button name={`INCRMENT`} action={this._increment} />
+        <Button name={`DECRMENT`} action={this._decrement} />
+
         <Route path="/" component={Navigation}></Route>
 
         <Route path="/want" component={Want}></Route>
@@ -21,6 +33,17 @@ class App extends React.Component {
       </div>
     );
   }
+
+  _increment = () => {
+    this.setState((prevState) => {
+      return { changeValue: prevState.changeValue + 1 };
+    });
+  };
+  _decrement = () => {
+    this.setState((prevState) => {
+      return { changeValue: prevState.changeValue - 1 };
+    });
+  };
 }
 
 export default App;
